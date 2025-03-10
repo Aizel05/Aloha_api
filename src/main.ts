7e713ac89e0fb74a61e5bce703bd03cdf1860f27
Aizel05 <aizelceleron@gmail.com>
2025-03-10 17:57:37 -0500

    Cambio_Error_6

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { randomUUID } from 'crypto';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   await app.listen(3000);
   
-  console.log('UUID Test:', randomUUID());
+
 }
 
 bootstrap();
