e59a3f219ca734b22ddd56a8cff0e31917ded03a
Aizel05 <aizelceleron@gmail.com>
2025-03-11 08:51:30 -0500

    Cambio_Error_8

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { AccessToken } from '../tokens/access-token.entity';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN', '1d') },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN', '3h') },
       }),
       inject: [ConfigService],
     }),
